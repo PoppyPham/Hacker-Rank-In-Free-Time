753b0fe394ef722b228de21d007800bc4cc54e98
Bill Pham <psb.jct@gmail.com>
2018-11-29 12:11:36 +0700

    Add Counting Valleys

---
+++ Interview Preparation Kit/Warmup-Challenges/Counting_Valleys.cs
@@ -0,0 +1,54 @@
+/* Problem:
+https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+*/
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, string s) {
+        int totalValey = 0;
+        int currentLevel = 0;
+        int seaLevel = 0;
+        
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == 'U')
+                currentLevel++;
+            if (s[i] == 'D')
+                currentLevel--;
+            if (currentLevel == seaLevel && s[i] == 'U')
+                totalValey ++;    
+        }
+        
+        return totalValey;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string s = Console.ReadLine();
+
+        int result = countingValleys(n, s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
--- Interview Preparation Kit/Warmup-Challenges/Sock_Merchant.cs
+++ Interview Preparation Kit/Warmup-Challenges/Sock_Merchant.cs
@@ -1,11 +1,3 @@
-John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
-
-For example, there are  socks with colors . There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
-
-Function Description
-
-Complete the sockMerchant function in the editor below. It must return an integer representing the number of matching pairs of socks that are available.
-
 /*
  Problem:
  https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
